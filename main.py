9b4bd481ed418b9a775c64aa8bf6e9fbf4e426a6
jirkakrepl <jiri.krepl@gmail.com>
2021-06-01 09:40:41 +0200

    feat: start/stop clean cycle

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from machine import UART
-# import time
+import time
 import struct
+
+START = 128
+FULL = 132
+STOP = 173
+CLEAN = 135
 
 # this uses the UART_1 default pins for TXD and RXD (``P3`` and ``P4``)
 uart = UART(1, 115200)
 
-uart.write(struct.pack("b", 128))
+uart.write(struct.pack("b", START))
 
-uart.write(struct.pack("b", 135))
+uart.write(struct.pack("b", FULL))
+
+uart.write(struct.pack("b", CLEAN))
+
+time.sleep(.5)
+
+uart.write(struct.pack("b", CLEAN))
+
+uart.write(struct.pack("b", STOP))
 
 print('done')
