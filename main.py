aa9c5423a42e236be7680ca8ee5512b1b4f73e00
jirkakrepl <jiri.krepl@gmail.com>
2021-06-02 14:36:17 +0200

    feat: roomba can rotate using drive PWD command

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 from machine import UART
 import time
 import struct
+import binascii
 
 START = 128
 FULL = 132
 STOP = 173
 CLEAN = 135
+DRIVE_PWM = 146
 
 # this uses the UART_1 default pins for TXD and RXD (``P3`` and ``P4``)
 uart = UART(1, 115200)
 
-uart.write(struct.pack("b", START))
+uart.write(struct.pack("B", START))
+uart.write(struct.pack("B", FULL))
+time.sleep(0.1)
 
-uart.write(struct.pack("b", FULL))
+# uart.write(struct.pack("B", CLEAN))
+# time.sleep(.5)
+# uart.write(struct.pack("B", CLEAN))
 
-uart.write(struct.pack("b", CLEAN))
+speed = 64
 
-time.sleep(.5)
+uart.write(struct.pack(">Bhh", DRIVE_PWM, speed, -speed))
+time.sleep(1)
 
-uart.write(struct.pack("b", CLEAN))
+uart.write(struct.pack(">Bhh", DRIVE_PWM, -speed, speed))
+time.sleep(1)
 
-uart.write(struct.pack("b", STOP))
+uart.write(struct.pack("B", STOP))
 
 print('done')
