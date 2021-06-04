15669a77c2032dca3ffab874f9cd0fba9de31f50
jirkakrepl <jiri.krepl@gmail.com>
2021-06-04 17:51:05 +0200

    feat: can print on led display

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ STOP = 173
 CLEAN = 135
 DRIVE_PWM = 146
 MOTORS_PWM = 144
+LEDS = 139
+DIGIT_LEDS_ASCII = 164
 
 # this uses the UART_1 default pins for TXD and RXD (``P3`` and ``P4``)
 uart = UART(1, 115200)
@@ -20,6 +22,10 @@ time.sleep(0.1)
 # time.sleep(.5)
 # uart.write(struct.pack("B", CLEAN))
 
+uart.write(struct.pack(">Bbbb", LEDS, 8, 255, 128))
+
+uart.write(struct.pack(">Bbbbb", DIGIT_LEDS_ASCII, 79, 83, 69, 76))
+
 uart.write(struct.pack(">Bbbb", MOTORS_PWM, 0, -64, 0))
 # uart.write(struct.pack(">Bbbb", MOTORS_PWM, 0, 0, 127))
 
@@ -31,6 +37,5 @@ time.sleep(1)
 uart.write(struct.pack(">Bhh", DRIVE_PWM, -speed, speed))
 time.sleep(1)
 
+uart.write(struct.pack(">Bbbbb", DIGIT_LEDS_ASCII, 83, 84, 79, 80))
 uart.write(struct.pack("B", STOP))
-
-print('done')
