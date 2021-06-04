cd2c13e3dd6c58365cbacbcee8591daaf34e9d25
jirkakrepl <jiri.krepl@gmail.com>
2021-06-04 17:45:05 +0200

    feat: command for operating vacuum motors

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from machine import UART
 import time
 import struct
-import binascii
 
 START = 128
 FULL = 132
 STOP = 173
 CLEAN = 135
 DRIVE_PWM = 146
+MOTORS_PWM = 144
 
 # this uses the UART_1 default pins for TXD and RXD (``P3`` and ``P4``)
 uart = UART(1, 115200)
@@ -19,6 +19,9 @@ time.sleep(0.1)
 # uart.write(struct.pack("B", CLEAN))
 # time.sleep(.5)
 # uart.write(struct.pack("B", CLEAN))
+
+uart.write(struct.pack(">Bbbb", MOTORS_PWM, 0, -64, 0))
+# uart.write(struct.pack(">Bbbb", MOTORS_PWM, 0, 0, 127))
 
 speed = 64
 
